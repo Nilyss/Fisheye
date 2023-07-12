815da3b5fafaf31a144b0c3d0c68a2d504dc7845
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2023-07-12 14:34:00 +0200

    Add "trapFocus" to modal & lightbox
    Create trapFocus function in Utils.js
    
    Call the function on ContactModal.js & Lightbox.js
    
    now the tab feature works fine on modal & lightbox

--- front_end/scripts/utils/Utils.js
+++ front_end/scripts/utils/Utils.js
@@ -7,7 +7,9 @@ class Utils {
   };
 
   findPhotographer = (photographers, id) => {
-    return photographers.find((photographer) => photographer.id === parseInt(id));
+    return photographers.find(
+      (photographer) => photographer.id === parseInt(id)
+    );
   };
 
   getTotalLike = (images, videos) => {
@@ -20,5 +22,38 @@ class Utils {
       0
     );
     return totalImagesLikes + totalVideosLikes;
+  };
+
+  trapFocus = (el) => {
+    // add all the elements inside modal which you want to make focusable
+    const focusableElements =
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+    const firstFocusableElement = el.querySelectorAll(focusableElements)[0];
+    const focusableContent = el.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+    document.addEventListener('keydown', function (e) {
+      let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus();
+          e.preventDefault();
+        }
+      } else {
+        // if tab key is pressed
+        if (document.activeElement === lastFocusableElement) {
+          firstFocusableElement.focus();
+          e.preventDefault();
+        }
+      }
+    });
+
+    firstFocusableElement.focus();
   };
 }
